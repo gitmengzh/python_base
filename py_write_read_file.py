11fd174655abe4d09c7ff766e39a3093f280dcf9
mengzh <mengzh1618@gmail.com>
2020-07-19 23:48:29 +0800

    python read&&write file  update

--- py_write_read_file.py
+++ py_write_read_file.py
@@ -7,7 +7,9 @@ mode常用的模式：
 r：表示文件只能读取
 w：表示文件只能写入
 a：表示打开文件，在原有内容的基础上追加内容，在末尾写入
-w+:表示可以对文件进行读写双重操作
+
+r+:
+a+:
 mode参数可以省略不填，默认为r模式mode参数还可以指定以什么样的编码方式读写文本，默认情况下open是以文本形式打开文件的，比如上面的四种mode模式。
 
 当你需要以字节（二进制）形式读写文件时，只需要在mode参数中追加'b'即可：
@@ -38,40 +40,63 @@ def read_txt_file3(path):
         content3_1 = f3.readline()
         content3_2 = f3.readline()
         print(content3_1, content3_2)
-
+# w mode， 重新写入内容
 def write_txt_file1(path):
     with open(path, 'w') as f4:
-        content4 = f4.write('111')
-        print(type(content4))
+        content4 = f4.write('111') # 返回为写入内容字节数
+        print(content4)
 
-
+# a mode, 追加内容
 def write_txt_file2(path):
     with open(path, 'a') as f5:
         content6 = f5.write('aaaa')
         print(content6)
 
-#
-def read_write_txt_file(path):
-    with open(path, 'w+') as f6:
+#a+ mode: 在后一行添加内容，指针一直在文件最后，想要读取内容，需要用seek
+#如果找不到该文件会在相应的路径下创建新文件
+def a_plus_mode(path):
+    with open(path, 'a+') as f6:
+        # f6.seek(0)
+        # content = f6.read()
+        f6.write('12345')
+        f6.seek(0)
         content7 = f6.read()
-        # f6.write('66666')
+        print(content7,'###')
+        f6.close()
 
-        print(content7)
+# w+ mode:试验结果，先对文件进行写操作，无论有没有write方法，都会清空内容（可以理解完全覆盖）
+# 如果找不到文件，会在路径中创建相应文件
+# 想要读取全部内容，也必须用seek方法将指针指到开始位置
+def w_plus_mode(path):
+    with open(path, 'w+') as f:
+        f.write('ssss')
+        f.seek(0)
+        content = f.read()
+        print(content)
+        f.close()
+
+# r+ mode, 1 如果先read,再write,那么read读取的为原来内容
+# 2 先write，再read，write将会替换掉相应个数的字符，read只能读取没有被替换的剩余字符 ，例如原值‘aaaa', write’vv'，则read内容为‘aa'
+# 第二种情况是因为read的指针是被替换之后的指针地址，如果想要获取写后的内容，通过seek方法，从初始地址开始读即可
+# 不能创建新文件，如果找不到改文件报错
+def r_plus_mode(path):
+    with open(path, 'r+') as f:
+        # content1 = f.read()
+        f.write('33')
+        f.seek(0)
+        content2 = f.read()
+        print('###',content2)
+        f.close()
 
 
-def wr_test():
-    f = open('C:/Users/meng4/Desktop/project/testfile/1.txt', 'r+')
-    # f.write('sss123')
-    content = f.read()
-    print(content)
-    f.close()
 
 
 if __name__ == "__main__":
-       file_path ='C:/Users/meng4/Desktop/project/testfile/1.txt'
+       file_path ='C:/Users/meng4/Desktop/project/testfile/4.txt'
        # read_txt_file(file_path)
        # read_txt_file3(file_path)
        # write_txt_file2(file_path)
-       read_write_txt_file(file_path)
-
-       # wr_test()
+       # read_write_txt_file(file_path)
+       # r_plus_mode(file_path)
+       # w_plus_mode(file_path)
+       a_plus_mode(file_path)

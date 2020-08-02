7708f9fc1b534340d97236be55d43b17efc0a3d9
韩斗 <18210850103@163.com>
2020-08-03 00:23:19 +0800

    create python json

---
+++ py_json.py
@@ -0,0 +1,37 @@
+# python 中json的序列化
+import json
+
+
+'''
+json 语法:
+
+    数据在名称/值对中
+    数据由逗号分隔
+    花括号保存对象, 
+    方括号保存数组
+    
+    JSON 值可以是：
+
+    数字（整数或浮点数）
+    字符串（在双引号中）
+    逻辑值（true 或 false）
+    数组（在方括号中）
+    对象（在花括号中）
+    null
+    对象和数组的使用区别
+
+'''
+json1 = [  ]
+json2 = []
+json3 = []
+json4 = []
+
+
+'''
+python 中关于json解析的方法：
+dumps()  将python对象编码成json字符串
+loads()  将json字符串解码为python对象
+dump()   将python内置类型编码成json字符串并写入文件
+load()   读取文件中的json并解码成python对象
+'''
+

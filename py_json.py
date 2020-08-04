8f28464f9537e745dcc7f1bac0f0032a002dc724
韩斗 <18210850103@163.com>
2020-08-05 07:34:03 +0800

    update json

--- py_json.py
+++ py_json.py
@@ -1,5 +1,7 @@
 # python 中json的序列化
-import json
+import json, sys, os
+path1 = os.pardir(os.path.dirname('..'))
+print(path1)
 
 
 '''
@@ -18,7 +20,10 @@ json 语法:
     数组（在方括号中）
     对象（在花括号中）
     null
-    对象和数组的使用区别
+    
+    json对象： 存储多个key/value, key 必须是字符串，
+    value 可以是合法的 JSON 数据类型（字符串, 数字, 对象, 数组, 布尔值或 null）
+    json数组: 数组值必须是合法的 JSON 数据类型（字符串, 数字, 对象, 数组, 布尔值或 null）
 
 '''
 json1 = [ {'aaa', 'bbb', 'ccc'},
@@ -39,4 +44,13 @@ loads()  将json字符串解码为python对象
 dump()   将python内置类型编码成json字符串并写入文件
 load()   读取文件中的json并解码成python对象
 '''
+
+def json_dumps1():
+    py_json1 = json.dumps(json3)
+    #json_py1 = json.loads({'name':'tom', 'age': 18})
+    print(py_json1)
+
+
+if __name__ == "__main__":
+    json_dumps1()
 

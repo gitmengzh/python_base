5274da26fc840f74efaab7de78a3f4017610a471
zhenweicui <18604287698@163.com>
2020-09-17 19:07:48 +0800

    split && strip 知识点

---
+++ py_split_strip.py
@@ -0,0 +1,17 @@
+'''
+介绍strip和split相关方法使用
+'''
+
+
+#
+def py_strip(s):
+    pass
+
+# str.split(s) 以s对str进行分割，返回一个list
+def py_split(s):
+    result1 = s.split()
+    result2 = s.split()
+    result3 = s.split()
+    result4 = s.split()
+    result5 = s.split()
+
